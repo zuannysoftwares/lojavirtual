e8cad15610729beba7eec13ee29b9cf322374c6e
zuannysoftwares <jonatarafael_zuanny@yahoo.com>
2017-01-20 23:38:15 -0200

    Teste Paginação

--- Zuanny.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
+++ Zuanny.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -14,7 +14,7 @@ namespace Zuanny.LojaVirtual.Web.HtmlHelpers
         {
             StringBuilder resultado = new StringBuilder();
 
-            for (int i = 1; i < paginacao.TotalPagina; i++)
+            for (int i = 1; i <= paginacao.TotalPagina; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.MergeAttribute("href", paginaUrl(i));
--- Zuanny.LojaVitual.UnitTest/UnitTest1.cs
+++ Zuanny.LojaVitual.UnitTest/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Web;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Zuanny.LojaVirtual.Web.HtmlHelpers;
+using Zuanny.LojaVirtual.Web.Models;
 
 namespace Zuanny.LojaVitual.UnitTest
 {
@@ -9,6 +13,24 @@ namespace Zuanny.LojaVitual.UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            HtmlHelper html = null;
+
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensPorPagina = 1,
+                ItensTotal = 5
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            MvcHtmlString resultado = html.PageLinks(paginacao, paginaUrl);
+
+            Assert.AreEqual(
+                @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                +@"<a class=""btn btn-default btn-primary selected"" href="" Pagina2"">2</a>"
+                +@"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                );
         }
     }
 }
